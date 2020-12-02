6e8fb69cd5131109a8fb02d192846d0094ab8e10
leosara2020 <73582810+leosara2020@users.noreply.github.com>
2020-12-02 15:43:46 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,28 @@
+class Chain {
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.5,
+            length:10
+        }
+        this.pointB=pointB;
+        this.chain=Constraint.create(options);
+        World.add(world,this.chain)
+    }
+    fly(){
+        this.chain.bodyA=null
+    }
+
+    display(){
+        if(this.chain.bodyA){
+
+        
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.pointB;
+        strokeWeight(5);
+        stroke("yellow");
+    line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+}
+}
